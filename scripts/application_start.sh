c89d00f3ffc1a09ab9c986f810a0ec089aab228f
tm-cheska-peralta <cheska.p@thinkingmachin.es>
2024-11-04 22:53:38 +0800

    fix start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,5 +11,7 @@ export DAGSTER_POSTGRES_USERNAME=$(aws ssm get-parameter --name "DAGSTER_POSTGRE
 export DAGSTER_POSTGRES_PASSWORD=$(aws ssm get-parameter --name "DAGSTER_POSTGRES_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
 export DAGSTER_POSTGRES_DB_NAME=$(aws ssm get-parameter --name "DAGSTER_POSTGRES_DB_NAME" --query "Parameter.Value" --output text)
 
+# Get ECR credentials
+aws ecr get-login-password --region $ECR_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
 # Build and Run
 sudo -E docker compose -f ci/docker-compose.yaml up -d --build
